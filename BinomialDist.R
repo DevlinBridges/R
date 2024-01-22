dd82034b6963e5b18d639c23b3ec4319055f1699
Devlin <48109348+DevlinBridges@users.noreply.github.com>
2024-01-22 11:34:25 -0500

    Create BinomialDist.R

---
+++ BinomialDist.R
@@ -0,0 +1,6 @@
+plot(success,dbinom(success,size=20,prob=.3),
++      type='h',
++      main='Binomial Distribution (n=20, p=0.3)',
++      ylab='Probability',
++      xlab ='# Successes',
++      lwd=3)
